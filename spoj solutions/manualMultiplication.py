c68cfc4b3e7961d333a5f0871c79aea9496934ba
ashutoshdhondkar <33003925+ashutoshdhondkar@users.noreply.github.com>
2018-03-24 01:57:08 +0530

    simulator of manual multiplication

---
+++ spoj solutions/manualMultiplication.py
@@ -0,0 +1,27 @@
+# manual multiplication
+
+num1=int(input("Enter num1 : "))
+num = input("Enter num2 : ")
+num2=[int(x) for x in list(num)]
+# store the second number in the form of list
+
+addition=0  
+deci=10 
+
+print(num1)
+print("*",num)
+
+if(len(num2)==1):   #if num2 is a 1 digit number
+    temp=num1 * num2[len(num2)-1]
+    print("-"*8)
+    print(temp)
+else:   #if num2 is  multidigit number
+    print("-"*8)
+    for x in range(len(num2)):
+        temp = num1 * num2[len(num2) -x-1]
+        temp=temp*(deci**x);
+        print(" "*(len(num2)-x-1),temp)
+        addition += temp
+
+    print("-"*8)
+    print(addition)
