19a3b56142476bf4a0393fa3c3ef31dec4781b40
ashutoshdhondkar <33003925+ashutoshdhondkar@users.noreply.github.com>
2018-04-25 20:42:23 +0530

    Update regexValidEmails.py

--- regexValidEmails.py
+++ regexValidEmails.py
@@ -4,7 +4,7 @@ import re
 
 emails = ["john@example.com", "pythonl985ist@python.org", "??%4ly@email.com",'abx@11.com']
 
-pattern = re.compile("^[a-zA-Z0-9.?]+@\w+.\w+")
+pattern = re.compile("^[a-zA-Z0-9.?]+@\w+\.\w+")
 
 for line in emails:
     match = re.search(pattern,line)
